e7750f411642b1abd9bb2db82e8b81bcc1b953d9
Tmy <tmy300803@gmail.com>
2025-07-08 11:19:43 +0700

    cập nhật

--- app.py
+++ app.py
@@ -620,8 +620,6 @@ def edit_blacklist_author(entry_id):
 #     print("🚀 Đang chạy Flask bằng Gunicorn")
 #     app.run(debug=True, host='0.0.0.0', port=5000)
 
-print("✅ Flask app được tạo:", app)
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
+    print("✅ Flask app được tạo:", app)
