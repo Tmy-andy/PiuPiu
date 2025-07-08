0bdc0ee31c5b658841e97f557e3337d043b18d94
Tmy <tmy300803@gmail.com>
2025-07-08 11:33:22 +0700

    cập nhật

--- app.py
+++ app.py
@@ -619,4 +619,4 @@ def edit_blacklist_author(entry_id):
 #     import os
 #     port = int(os.environ.get('PORT', 5000))
 #     app.run(debug=True, host='0.0.0.0', port=port)
-    print(f"🚀 Đang chạy Flask trên cổng {port}")
+# print(f"🚀 Đang chạy Flask trên cổng {port}")
