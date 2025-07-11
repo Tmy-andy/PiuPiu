d997bfb20f2b75b3f326279c0f48ea98a109a161
Tmy <tmy300803@gmail.com>
2025-07-11 10:13:47 +0700

    cập nhật

--- app.py
+++ app.py
@@ -733,7 +733,7 @@ def delete_ability(ability_id):
 @app.route('/kim_bai')
 @login_required
 def kim_bai():
-    members = User.query.filter_by(role='member').order_by(User.display_name).all()
+    members = User.query.order_by(User.display_name).all()
     return render_template('kim_bai.html', members=members)
 
 @app.route('/increase_death/<int:user_id>', methods=['POST'])
