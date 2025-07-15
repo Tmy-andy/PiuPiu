69f4693e039d2abc61c989122a1a69916c9c6cfc
Tmy <tmy300803@gmail.com>
2025-07-15 13:11:18 +0700

    add activity_log

--- models.py
+++ models.py
@@ -96,7 +96,7 @@ class PlayerOffRequest(db.Model):
 
 class ActivityLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     action = db.Column(db.String(255), nullable=False)
     detail = db.Column(db.Text, nullable=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
