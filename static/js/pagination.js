743671cf916e6c7c80ce324c1e62a14017c1d0ae
Tmy <tmy300803@gmail.com>
2025-07-23 12:47:35 +0700

    Tối ưu phân trang

---
+++ static/js/pagination.js
@@ -0,0 +1,30 @@
+function setupAjaxPagination(tableSelector, urlBase) {
+    function reloadTable(page = 1) {
+        fetch(`${urlBase}?page=${page}`, {
+            headers: { 'X-Requested-With': 'XMLHttpRequest' }
+        })
+        .then(res => res.json())
+        .then(data => {
+            const tbody = document.querySelector(`${tableSelector} tbody`);
+            if (tbody) tbody.innerHTML = data.rows;
+
+            const pagination = document.querySelector('#pagination-container');
+            if (pagination) pagination.innerHTML = data.pagination;
+
+            attachEvents();
+        })
+        .catch(err => console.error('Lỗi load bảng:', err));
+    }
+
+    function attachEvents() {
+        document.querySelectorAll('.pagination-link').forEach(link => {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                const page = this.dataset.page;
+                reloadTable(page);
+            });
+        });
+    }
+
+    attachEvents();
+}
